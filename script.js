70bfa6357cb1a67a01d10869b9925b91c59da097
Toms Bude <87808350+ApnikaPanika@users.noreply.github.com>
2024-12-11 10:25:15 +0200

    Pievienoju failus

---
+++ script.js
@@ -0,0 +1,6 @@
+const button = document.querySelector('.present')
+
+button.addEventListener('mouseover', () => {
+    window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley", '_blank');
+
+})
